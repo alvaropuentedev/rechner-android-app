f5f13fd9dc033e8cdf6d2fc76a075a5d6d5e409e
Alvaro Puente <alvaropuentedev@gmail.com>
2025-06-04 01:02:17 +0200

    Bump version from 1.0.2 to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.alvaropuente.rechnerandroidapp"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
